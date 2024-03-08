457f58cb890354c83302f24a422d4b314d7b8c5e
vadim-ma <color.ui>
2024-03-08 18:39:23 +0300

    feat: implement getDayName

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -46,8 +46,9 @@ function getTime(date) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const formatter = new Intl.DateTimeFormat('en-US', { weekday: 'long' });
+  return formatter.format(new Date(date));
 }
 
 /**
