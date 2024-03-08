831ae5a9616d73282e48d0f867d2ec21fce63932
vadim-ma <color.ui>
2024-03-08 20:01:53 +0300

    feat: implement isDateInPeriod

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -126,8 +126,12 @@ function getCountDaysOnPeriod(dateStart, dateEnd) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const { start, end } = period;
+  const startT = new Date(start).getTime();
+  const endT = new Date(end).getTime();
+  const dateT = new Date(date).getTime();
+  return startT <= dateT && dateT <= endT;
 }
 
 /**
